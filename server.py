7240cf576b30c3f3ad42ed603fa265783a506193
Adwaith S <adwaiths.revathi@gmail.com>
2023-04-16 10:32:06 +0530

    fixed app.html and reverted server.py to original

--- server.py
+++ server.py
@@ -1,28 +1,9 @@
 from fastapi import FastAPI
 import json
-from fastapi_socketio import SocketManager
-import socketio
-
 
 app = FastAPI()
-sio = socketio.AsyncServer(async_mode='asgi')    
-socket_app = socketio.ASGIApp(sio, static_files={'/': 'app.html'})    
-background_task_started = False    
-    
-    
-async def background_task():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-        await sio.emit('data', data)
-
-@sio.on('disconnect')
-def test_disconnect(sid):
-    print('Client disconnected')
 
 
-@app.get("/hello")
+@app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-app.mount('/', socket_app)
+    return json.load(open('data.json'))
