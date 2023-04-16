1ec900210b5058116abc659c189e0405f1b3d05b
Adwaith S <adwaiths.revathi@gmail.com>
2023-04-16 09:04:04 +0530

    Added dataset and app.html

--- server.py
+++ server.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
 import json
+from fastapi_socketio import SocketManager
+import socketio
+
 
 app = FastAPI()
+sio = socketio.AsyncServer(async_mode='asgi')    
+socket_app = socketio.ASGIApp(sio, static_files={'/': 'app.html'})    
+background_task_started = False    
+    
+    
+async def background_task():
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        await sio.emit('data', data)
+
+@sio.on('disconnect')
+def test_disconnect(sid):
+    print('Client disconnected')
 
 
-@app.get("/")
+@app.get("/hello")
 async def root():
-    return json.load(open('data.json'))
+    return {"message": "Hello World"}
+
+
+app.mount('/', socket_app)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import cv2
 from ultralytics import YOLO
-
+import base64
 import json
 
 conf = 0.0
@@ -9,7 +9,7 @@ isWearingHardHat = False
 model = YOLO('models/yolo8n-hard-hat-det.pt') # Fastest and more accurate
 
 # Open the video file
-video_path = "Dataset/videos/4.mp4"
+video_path = "Dataset/videos/2.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Loop through the video frames
@@ -19,18 +19,24 @@ while cap.isOpened():
        
     if success:
         # Run YOLOv8 inference on the frame
-        results = model.predict(frame, save_conf=True)[0]
-        if results.boxes.shape[0] != 0:
-            conf = float(results.boxes.conf)
-            isWearingHardHat = True if int(results.boxes.cls) == 0 else False
-            print(conf, isWearingHardHat)
-            json.dump({'conf': conf, 'isWearingHardHat': isWearingHardHat}, open('data.json', 'w'))
-    
-        # Visualize the results on the frame
-        annotated_frame = results.plot()
-
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        results = model.predict(frame, save_conf=True, stream=True)
+        for result in results:
+            if result.boxes.shape[0] != 0:
+                conf = float(result.boxes.conf[0])
+                isWearingHardHat = True if int(result.boxes.cls[0]) == 0 else False
+                print(conf, isWearingHardHat)
+        
+            # Visualize the results on the frame
+            annotated_frame = result.plot()
+            
+            retval, buffer = cv2.imencode('.png', annotated_frame)
+            png_as_text = base64.b64encode(buffer)
+            
+            with open('./data.json', 'w') as f:
+                json.dump({'conf': conf, 'isWearingHardHat': isWearingHardHat, 'image': png_as_text.decode('utf-8')}, f)
+            
+            # Display the annotated frame
+            cv2.imshow("Annotated Frame", annotated_frame)
 
         key = cv2.waitKey(1)    
         # Break the loop if 'q' is pressed
